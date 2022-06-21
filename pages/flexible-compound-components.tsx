1956e2e100f4139ae8f2f392cc8a248dd3899627
YofretRios <riosmerca28@gmail.com>
2022-06-20 19:38:27 -0500

    Switch from Top Level API to props

--- pages/flexible-compound-components.tsx
+++ pages/flexible-compound-components.tsx
@@ -1,10 +1,18 @@
-import { FC, useState, useContext, createContext, Children, cloneElement, ReactElement } from "react";
+import {
+  FC,
+  useState,
+  useContext,
+  createContext,
+  Children,
+  cloneElement,
+  ReactElement,
+} from "react";
 import type { NextPage } from "next";
 import Switch from "../components/Switch";
 
 interface IToggleContext {
-  on: boolean;
-  onToggle: () => void;
+  on?: boolean;
+  onToggle?: () => void;
 }
 
 type AuxElement = {
@@ -17,34 +25,69 @@ type TogglerProps = {
   onToggle?: () => {};
 };
 
+// TODO Create ToggleContext Here with createContext
+const ToggleContext = createContext<IToggleContext | null>(null);
+ToggleContext.displayName = "ToggleContext";
+
 const Toggle: FC<AuxElement> = ({ children }) => {
   const [on, setOn] = useState(false);
   const onToggle = () => setOn((on) => !on);
 
+  // TODO instead of iterating over children using Children.map and returning de value, return <ToggleContext.Provider> passing down
+  // as value {on, onToggle}
+  // return (
+  //   <>
+  //     {Children.map(children, (child) =>
+  //       typeof child.type !== "string"
+  //         ? cloneElement(child, { on, onToggle })
+  //         : child
+  //     )}
+  //   </>
+  // );
+
   return (
-    <>
-      {Children.map(children, (child) =>
-        typeof child.type !== "string"
-          ? cloneElement(child, { on, onToggle })
-          : child
-      )}
-    </>
+    <ToggleContext.Provider value={{ on, onToggle }}>
+      {children}
+    </ToggleContext.Provider>
   );
 };
 
-const ToggleOn = ({ on, children }: TogglerProps) => {
+// TODO: Create a custom hook to use the ToggleContext
+function useToggleContext(): IToggleContext {
+  const context = useContext(ToggleContext);
+
+  if (context === null) {
+    throw new Error("useToggleContext must be used within a Toggle component");
+  }
+
+  return context;
+}
+
+// TODO: Switch from props to context
+const ToggleOn = ({ children }: TogglerProps) => {
+  const { on } = useToggleContext();
+
   return <>{on ? children : null}</>;
 };
 
-const ToggleOff = ({ on, children }: TogglerProps) => {
+// TODO: Switch from props to context
+const ToggleOff = ({ children }: TogglerProps) => {
+  const { on } = useToggleContext();
+  
   return <>{!on ? children : null}</>;
 };
 
-const ToggleStatus = ({ on }: TogglerProps) => {
+// TODO: Switch from props to context
+const ToggleStatus = () => {
+  const { on } = useToggleContext();
+
   return <>{!on ? "Switch is off" : "Switch is on"}</>;
 };
 
-const ToggleButton = ({ on, onToggle }: TogglerProps) => {
+// TODO: Switch from props to context
+const ToggleButton = () => {
+  const { on, onToggle } = useToggleContext();
+
   return <Switch aria-label="toggle" on={on} onClick={onToggle} />;
 };
 
